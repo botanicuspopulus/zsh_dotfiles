b365bcbe45d83f7a69ca87b8006d4bee2fd6e445
William Smith <william@williamgsmith.co.za>
2023-11-06 00:26:47 +0200

    Execution permissions. Updates to plugins and completion

---
+++ conf.d/zsh-options.zsh
@@ -0,0 +1,5 @@
+setopt NO_BG_NICE # don't nice background tasks
+setopt NO_HUP
+setopt NO_LIST_BEEP
+setopt LOCAL_OPTIONS # allow function to have local options
+setopt LOCAL_TRAPS # allow functions to have local traps
