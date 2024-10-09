98377a7b08245ad45817e864ac1a79c7ff694611
William Smith <william@williamgsmith.co.za>
2024-10-09 18:07:40 +0200

    fix: Removing export TERMINAL

--- dot-config/zsh/conf.d/exports.zsh
+++ dot-config/zsh/conf.d/exports.zsh
@@ -1,5 +1,3 @@
-export TERMINAL=alacritty
-
 PERL5LIB="$HOME/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
 PERL_LOCAL_LIB_ROOT="$HOME/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
 PERL_MB_OPT="--install_base \"$HOME/perl5\""; export PERL_MB_OPT;
